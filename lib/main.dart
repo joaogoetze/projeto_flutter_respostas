ccda6d42d1ad154df837cd05f752dcaf1b90330b
joaoaugusto88 <82613873+joaoaugusto88@users.noreply.github.com>
2023-05-26 23:32:00 -0300

    Atualização
    Desenvolvimento conforme as aulas

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() {
+// Primeira coisa que é executada no app
+void main()
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class _PerguntasAppState extends State<MyApp>
+{
+    var _perguntaSelecionada = 0;
+
+    void _responder()
+    {
+      _perguntaSelecionada++;
+      print(_perguntaSelecionada);
+    }
+
+    @override
+    Widget build(BuildContext context) {
+
+      final List<String> perguntas =
+      [
+        'Qual a sua cor favorita?',
+        'Qual o seu animal favorito?',
+      ];
+
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Pergutnas'),
+          ),
+          // Column server para que seja possível adicionar mais de um widget dentro de outro
+          body: Column(
+            children: <Widget>[
+              Text(perguntas[perguntaSelecionada]),
+              ElevatedButton(
+                child: Text('Resposta 1'),
+                onPressed: _responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 2'),
+                onPressed: _responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 3'),
+                onPressed: _responder,
+              ),
+            ],
+          ),
+        ),
+
+      );
+    }
+}
+
+// O "extends StatelessWidget" é o que torna a classe "MyApp" em widget
+class MyApp extends StatefulWidget
+{
+  // Não sei o que isso faz
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    _PerguntasAppState createState()
+    {
+        return new _PerguntasAppState();
+    }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
