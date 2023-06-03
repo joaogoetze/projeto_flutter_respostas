cb7d182043d7f4d748a6e66b3be2dad41386b282
joaoaugusto88 <82613873+joaoaugusto88@users.noreply.github.com>
2023-06-03 19:15:54 -0300

    Comentários
    Adição de comentários para melhor entendimento do código.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 // Primeira coisa que é executada no app
 void main()
@@ -8,8 +9,11 @@ void main()
 
 class _PerguntasAppState extends State<MyApp>
 {
+    // Começa em 0 como padrão - 1º pergunta
     var _perguntaSelecionada = 0;
 
+    // Quando o usuário clica em um botão de resposta
+    // aciona essa função que muda a pergunta selecionada
     void _responder()
     {
       _perguntaSelecionada++;
@@ -19,12 +23,14 @@ class _PerguntasAppState extends State<MyApp>
     @override
     Widget build(BuildContext context) {
 
+      // Lista de perguntas
       final List<String> perguntas =
       [
         'Qual a sua cor favorita?',
         'Qual o seu animal favorito?',
       ];
 
+      // Padrão de estrutura do Flutter
       return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
@@ -33,7 +39,7 @@ class _PerguntasAppState extends State<MyApp>
           // Column server para que seja possível adicionar mais de um widget dentro de outro
           body: Column(
             children: <Widget>[
-              Text(perguntas[perguntaSelecionada]),
+              Questao(perguntas[_perguntaSelecionada]),
               ElevatedButton(
                 child: Text('Resposta 1'),
                 onPressed: _responder,
@@ -49,7 +55,6 @@ class _PerguntasAppState extends State<MyApp>
             ],
           ),
         ),
-
       );
     }
 }
@@ -60,9 +65,9 @@ class MyApp extends StatefulWidget
   // Não sei o que isso faz
   const MyApp({super.key});
 
+  // Chama a classe no início do código que mostra as coisas na tela
     _PerguntasAppState createState()
     {
         return new _PerguntasAppState();
     }
-
 }
